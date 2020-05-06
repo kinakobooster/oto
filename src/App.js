34c28422bf7ef9a535f700acf105baab3a093e6f
Torii Megumi <torii.megumi@gmail.com>
2020-05-06 17:50:43 +0900

    作曲機能を実装する

--- src/App.js
+++ src/App.js
@@ -5,12 +5,17 @@ import * as Tone from 'tone'
 const synth = new Tone.PolySynth().toMaster()
 
 const makeSomeNoise = (note) => synth.triggerAttackRelease(note,"4n")
-const makeSomeChord = (array) => synth.triggerAttackRelease(array,"4n")
+const makeSomeChord = (array) => synth.triggerAttackRelease(array,"2n")
 const transposeNoteFromC4 = (number) => Tone.Frequency("C3").transpose(number).toNote()
 const transposeNoteFromKey = (number, keyNote) => Tone.Frequency(keyNote).transpose(number).toNote()
 let keyNote = "C3"
+const defaultBpm = 144
+Tone.Transport.bpm.value = defaultBpm
+
 
 function App() {
+  const score = []
+
   const scale = [0,2,4,5,7,9,11] // length 7
   const scaleNotes = scale.map((e) => transposeNoteFromKey(e, "C3"))
   const scaleNotesFromKey = (keyNote) = scale.map((e) => transposeNoteFromKey(e, keyNote))
@@ -30,11 +35,28 @@ function App() {
   const isDiatonic = (array) => {
     const scale = scaleNotes.map((e) => e.slice(0,-1)) // 最後の数字を取る（オクターブ違い対策）
     const strippedchord = array.map((e) => e.slice(0,-1)).filter((e) => (!scale.includes(e))) //スケールに含まれる音を取る
-    return strippedchord.length == 0
+    return strippedchord.length === 0
   }
 
+  //楽譜を触る
+  const soundFromStack = () => {
+    if (score.length !== 0) {
+      makeSomeChord(score.shift().sound)
+    }
+  }
+  const pushToScore = (array, chordName) => score.push({chordName:chordName, sound: array})
+  const loop = () => setInterval(() => {
+    console.log('beats!')
+    soundFromStack(score)
+    }, 2 / defaultBpm * 60 * 1000)
+  const scoreVisible = score.map((e) => e.chordName)
   return (
     <>
+    <button
+    onClick={() => loop()}
+    >start</button>
+    <h2>score</h2>
+    {scoreVisible.join()}
     <h2>note</h2>
     {scaleNotes.map((e) => {
       return <button onClick={() => makeSomeNoise(e)} >{e}</button>})}
@@ -44,9 +66,9 @@ function App() {
       const harmony2nd = majorHarmony2nd(e)
       const harmony3rd = majorHarmony3rd(e)
       return <div className={isDiatonic(harmony) ? 'blue' : '' }>
-      <button onClick={() => makeSomeChord(harmony)} >{e}</button>
-      <button onClick={() => makeSomeChord(harmony2nd)} >{e}.2nd</button>
-      <button onClick={() => makeSomeChord(harmony3rd)} >{e}.3rd</button>
+      <button onClick={() => pushToScore(harmony, e)} >{e}</button>
+      <button onClick={() => pushToScore(harmony2nd, e)} >{e}.2nd</button>
+      <button onClick={() => pushToScore(harmony3rd, e)} >{e}.3rd</button>
       </div>
       })}
     <h2>minor chord</h2>
@@ -55,9 +77,9 @@ function App() {
       const harmony2nd = minorHarmony2nd(e)
       const harmony3rd = minorHarmony3rd(e)
       return <div className={isDiatonic(harmony) ? 'blue' : '' }>
-      <button onClick={() => makeSomeChord(harmony)} >{e}m</button>
-      <button onClick={() => makeSomeChord(harmony2nd)} >{e}m.2nd</button>
-      <button onClick={() => makeSomeChord(harmony3rd)} >{e}m.3rd</button>
+      <button onClick={() => pushToScore(harmony)} >{e}m</button>
+      <button onClick={() => pushToScore(harmony2nd)} >{e}m.2nd</button>
+      <button onClick={() => pushToScore(harmony3rd)} >{e}m.3rd</button>
       </div>
       })}
     </>
